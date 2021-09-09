b9176f8a428896b9db198101159eb69a3a216bf5
Joao Pedro Mairinque <jpmairinque@gmail.com>
2021-09-09 19:28:03 -0300

    form validation

--- src/screens/SignUp/SignUp.js
+++ src/screens/SignUp/SignUp.js
@@ -5,49 +5,78 @@ import { TouchableOpacity } from 'react-native'
 
 const SignUp = ({navigation}) => {
 
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const [passwordConf,setPasswordConf ] = useState()
-    const [error, setError] = useState()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordConf,setPasswordConf ] = useState("")
+    const [error, setError] = useState("Por favor, informe alguns dados para que possamos te cadastrar")
 
     const handleSubmit = () => {
 
         var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
 
-        if(password != passwordConf){
-            setError("As suas senhas não coincidem =(")
-        }
-        if(password != passwordConf){
-            setError("As suas senhas não coincidem =(")
+        if(!email.match(pattern)){
+            setError("Insira um endereço de email válido")
+            return
         }
 
+        if(password != passwordConf){
+            setError("As suas senhas não coincidem =(")
+            return
+        }
         
+        if(password.length < 6){
+            setError("Sua senha deve ter no mínimo 6 caracteres")
+            return
+        }
+
+        navigation.navigate('Login')        
 
     }
 
 
     return (
         <S.FormBox>
+ 
         <S.FieldBox>
-        <S.Text style={{textAlign:'center'}} font="Montserrat-Bold" color={colors.purple} size="25px">Olá, como vai?</S.Text>
+        <S.Text 
+        style={{textAlign:'center'}} 
+        font="Montserrat-Bold" 
+        color={colors.purple} 
+        size="25px">Olá, como vai?</S.Text>
         
-        <S.Text style={{textAlign:'center', marginTop:20}} font="Montserrat-Regular" color={colors.purple} size="15px"
-        >Por favor, informe alguns dados para que possamos te cadastrar</S.Text>
+        <S.Text 
+        style={{textAlign:'center', marginTop:20}} 
+        font="Montserrat-Regular" 
+        color={colors.purple}
+         size="15px"
+        >{error}</S.Text>
         </S.FieldBox>
         <S.FieldBox>
-                <S.Text font="Montserrat-Bold" color={colors.purple} size="15px">Email</S.Text>
-                <S.Field onChange={(e)=>{setEmail(e)}} />
+                <S.Text 
+                font="Montserrat-Bold" 
+                color={colors.purple} 
+                size="15px">Email</S.Text>
+                <S.Field onChangeText={(e)=>{setEmail(e)}} />
         </S.FieldBox>
         <S.FieldBox>
-                <S.Text font="Montserrat-Bold" color={colors.purple} size="15px">Senha</S.Text>
-                <S.Field onChange={(e)=>{setPassword(e)}} />
+                <S.Text 
+                font="Montserrat-Bold" 
+                color={colors.purple} 
+                size="15px">Senha</S.Text>
+                <S.Field onChangeText={(e)=>{setPassword(e)}} />
         </S.FieldBox>
         <S.FieldBox>
-                <S.Text font="Montserrat-Bold" color={colors.purple} size="15px">Confirmar senha</S.Text>
-                <S.Field onChange={(e)=>{setPasswordConf(e)}}/>
+                <S.Text 
+                font="Montserrat-Bold" 
+                color={colors.purple} 
+                size="15px">Confirmar senha</S.Text>
+                <S.Field onChangeText={(e)=>{setPasswordConf(e)}}/>
         </S.FieldBox>
-        <S.Button>
-            <S.Text color={colors.white} font="Montserrat-Medium" size="20px">Cadastrar</S.Text>
+        <S.Button onPress={handleSubmit}>
+            <S.Text  
+            color={colors.white} 
+            font="Montserrat-Medium" 
+            size="20px">Cadastrar</S.Text>
         </S.Button>
         <TouchableOpacity onPress={()=>{navigation.navigate('Login')}}>
                 <S.Text 
@@ -55,7 +84,7 @@ const SignUp = ({navigation}) => {
                 font="Montserrat-Bold" color={colors.purple} 
                 size="15px">Voltar ao login</S.Text>
                </TouchableOpacity>
-
+               
         </S.FormBox>
     )
 }
